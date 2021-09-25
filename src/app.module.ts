e3e70ff7b22d6fb1df5433aa74d89e8aa4dc1239
Igor <igor_csns_s2@hotmail.com>
2021-09-25 18:38:48 -0300

    (feat):  synchronize and logging to TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,12 @@ import { ItemsModule } from './items/items.module';
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
       entities: [Item],
-      synchronize: true,
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true' ? true : false,
+      logging: process.env.TYPEORM_LOGGING === 'true' ? true : false,
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : false,
     }),
     ItemsModule,
   ],
